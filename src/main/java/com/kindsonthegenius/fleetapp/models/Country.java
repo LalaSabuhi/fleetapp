68ce706f704ce82d856e2669b56ed0f7b4e2e536
Lale Mammadli <yavuzadpu@gmail.com>
2024-04-05 05:28:54 +0400

    Initial commit

---
+++ src/main/java/com/kindsonthegenius/fleetapp/models/Country.java
@@ -0,0 +1,31 @@
+package com.kindsonthegenius.fleetapp.models;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Country {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String code;
+	private String capital;
+	private String description;
+	private String nationality;
+	private String continent;
+	
+	@OneToMany(mappedBy="country")
+	private List<State> states;
+}
---
+++ src/main/java/com/kindsonthegenius/fleetapp/models/User.java
@@ -0,0 +1,25 @@
+package com.kindsonthegenius.fleetapp.models;
+
+import jakarta.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Table(name = "app_users")
+public class User {
+		
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+	private int id;
+	private String username;
+	private String password;
+}
